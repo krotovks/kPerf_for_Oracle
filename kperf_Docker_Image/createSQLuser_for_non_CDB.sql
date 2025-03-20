cffc94281982cf2cca8f5c68fb2f81f652b34701
Konstantin <krotovks@gmail.com>
2025-03-20 17:16:49 +0500

    bug fix 2.1.0

--- kperf_Docker_Image/createSQLuser_for_non_CDB.sql
+++
@@ -1,6 +0,0 @@
-create user kperf identified by "Test1234!";
-grant create session to c##kperf;
-grant select on V_$SYSTEM_EVENT to kperf;
-grant select on v_$sysstat to kperf;
-grant select on v_$sqlarea to kperf;
-grant select on V_$SYS_TIME_MODEL to kperf;
